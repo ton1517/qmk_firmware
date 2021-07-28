d7b618573170a3dcdbddeecd3e85eb2addcb4d7a
ton1517 <tonton1517@gmail.com>
2021-07-28 20:15:46 +0900

    add my keymap.

---
+++ keyboards/claw44/keymaps/ton1517/README.md
@@ -0,0 +1,18 @@
+# claw44/rev1:ton1517
+
+## compile or flash
+
+```
+qmk flash -kb claw44/rev1 -km ton1517
+```
+
+```
+qmk flash -kb claw44/rev1 -km ton1517
+```
+
+
+## link
+
+* [qmk_firmware](https://github.com/qmk/qmk_firmware)
+* [VIA](https://caniusevia.com/)
+* [Remap](https://remap-keys.app/)
---
+++ keyboards/claw44/keymaps/ton1517/keymap.c
@@ -0,0 +1,136 @@
+/*
+Copyright 2012 Jun Wako <wakojun@gmail.com>
+Copyright 2015 Jack Humbert
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include QMK_KEYBOARD_H
+
+// Each layer gets a name for readability, which is then used in the keymap matrix below.
+// The underscores don't mean anything - you can have a layer called STUFF or any other name.
+// Layer names don't all need to be of the same length, obviously, and you can also skip them
+// entirely and just use numbers.
+
+enum layer_number {
+    _QWERTY = 0,
+    _RAISE,
+    _LOWER,
+    _ADJUST,
+};
+
+#define KC__EN LT(_RAISE, KC_LANG2)
+#define KC__JA LT(_LOWER, KC_LANG1)
+#define KC__SPC LT(_ADJUST, KC_SPC)
+
+// clang-format off
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [_QWERTY] = LAYOUT(
+    //,--------+--------+---------+--------+---------+--------.   ,--------+---------+--------+---------+--------+--------.
+       KC_TAB , KC_Q   , KC_W    , KC_E   , KC_R    , KC_T   ,     KC_Y   , KC_U    , KC_I   , KC_O    , KC_P   , KC_MINS,
+    //|--------+--------+---------+--------+---------+--------|   |--------+---------+--------+---------+--------+--------|
+       KC_LCTL, KC_A   , KC_S    , KC_D   , KC_F    , KC_G   ,     KC_H   , KC_J    , KC_K   , KC_L    , KC_SCLN, KC_QUOT,
+    //|--------+--------+---------+--------+---------+--------|   |--------+---------+--------+---------+--------+--------|
+       KC_LSFT, KC_Z   , KC_X    , KC_C   , KC_V    , KC_B   ,     KC_N   , KC_M    , KC_COMM, KC_DOT  , KC_SLSH, KC_BSLS,
+    //`--------+--------+---------+--------+---------+--------/   \--------+---------+--------+---------+--------+--------'
+                         KC_LALT , KC_LGUI, KC__EN  , KC_LSFT,     KC_ESC , KC__JA  , KC__SPC, KC_ENT
+    //                 `----------+--------+---------+--------'   `--------+---------+--------+---------'
+    ),
+
+    [_RAISE] = LAYOUT(
+    //,--------+--------+--------+--------+--------+--------.   ,--------+--------+--------+--------+--------+--------.
+       KC_GRV , KC_EXLM, KC_AT  , KC_HASH, KC_DLR , KC_PERC,     KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_UNDS,
+    //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------|
+       KC_LCTL, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,     KC_LCBR, KC_RCBR, KC_EQL , KC_PLUS, KC_COLN, KC_DQUO,
+    //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------|
+       KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,     KC_LBRC, KC_RBRC, KC_LT  , KC_GT  , KC_QUES, KC_PIPE,
+
+    //`--------+--------+--------+--------+--------+--------/   \--------+--------+--------+--------+--------+--------'
+                         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,     XXXXXXX, KC_RSFT, XXXXXXX, XXXXXXX
+    //                  `--------+--------+--------+--------'   `--------+--------+--------+--------'
+    ),
+
+    [_LOWER] = LAYOUT(
+    //,--------+--------+--------+--------+--------+--------.   ,--------+--------+--------+--------+--------+--------.
+       XXXXXXX, KC_P1  , KC_P2  , KC_P3  , KC_P4  , KC_P5  ,     KC_P6  , KC_P7  , KC_P8  , KC_P9  , KC_P0  , XXXXXXX,
+    //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------|
+       KC_LCTL, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,     XXXXXXX, KC_P4  , KC_P5  , KC_P6  , KC_PMNS, KC_PSLS,
+    //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------|
+       KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,     XXXXXXX, KC_P1  , KC_P2  , KC_P3  , KC_PPLS, KC_PAST,
+    //`--------+--------+--------+--------+--------+--------/   \--------+--------+--------+--------+--------+--------'
+                         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,     XXXXXXX, XXXXXXX, KC_PEQL, KC_P0
+    //                  `--------+--------+--------+--------'   `--------+--------+--------+--------'
+    ),
+
+    [_ADJUST] = LAYOUT(
+    //,--------+--------+--------+--------+--------+--------.   ,--------+--------+--------+--------+--------+--------.
+       KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  ,     KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 ,
+    //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------|
+       KC_LCTL, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,     KC_LEFT, KC_DOWN, KC_UP  , KC_DOWN, XXXXXXX, XXXXXXX,
+    //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------|
+       KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    //`--------+--------+--------+--------+--------+--------/   \--------+--------+--------+--------+--------+--------'
+                         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
+    //                  `--------+--------+--------+--------'   `--------+--------+--------+--------'
+    ),
+};
+// clang-format on
+
+uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        // スペースキーのみ長めに設定する
+        case LT(3, KC_SPACE):
+            return 200;
+        default:
+            return 120;
+    }
+}
+
+bool get_retro_tapping(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        // スペースキーのみRETRO_TAPPINGを有効にする
+        case LT(3, KC_SPACE):
+            return true;
+        default:
+            return false;
+    }
+}
+
+static bool ctrl_pressed = false;
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        // Ctrlの状態を保存する
+        case KC_LCTRL:
+            ctrl_pressed = record->event.pressed;
+            return true;
+
+        // Ctrl-Minus を押すと Ctrl-[ (vimでのesc) になるようにする
+        case KC_MINUS:
+            if (!ctrl_pressed) {
+                return true;
+            }
+
+            if (record->event.pressed) {
+                unregister_code(KC_MINUS);
+                register_code(KC_LBRACKET);
+            } else {
+                unregister_code(KC_LBRACKET);
+                if (ctrl_pressed) {
+                    register_code(KC_LCTRL);
+                }
+            }
+            return false;
+
+        default:
+            return true;
+    }
+}
