1d4d7c540d67a1c7a34479cca3fdb4ab6efde52b
ton1517 <tonton1517@gmail.com>
2021-08-15 18:47:53 +0900

    update lower layer keymap.

--- keyboards/claw44/keymaps/ton1517/keymap.c
+++ keyboards/claw44/keymaps/ton1517/keymap.c
@@ -62,11 +62,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     //,--------+--------+--------+--------+--------+--------.   ,--------+--------+--------+--------+--------+--------.
        XXXXXXX, KC_P1  , KC_P2  , KC_P3  , KC_P4  , KC_P5  ,     KC_P6  , KC_P7  , KC_P8  , KC_P9  , KC_P0  , XXXXXXX,
     //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------|
-       KC_LCTL, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,     XXXXXXX, KC_P4  , KC_P5  , KC_P6  , KC_PMNS, KC_PSLS,
+       KC_LCTL, KC_P1  , KC_P2  , KC_P3  , KC_P4  , KC_P5  ,     KC_P6  , KC_P7  , KC_P8  , KC_P9  , KC_P0  , XXXXXXX,
     //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------|
-       KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,     XXXXXXX, KC_P1  , KC_P2  , KC_P3  , KC_PPLS, KC_PAST,
+       KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,     XXXXXXX, XXXXXXX, KC_COMM, KC_DOT , XXXXXXX, XXXXXXX,
     //`--------+--------+--------+--------+--------+--------/   \--------+--------+--------+--------+--------+--------'
-                         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,     XXXXXXX, XXXXXXX, KC_PEQL, KC_P0
+                         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
     //                  `--------+--------+--------+--------'   `--------+--------+--------+--------'
     ),
 
